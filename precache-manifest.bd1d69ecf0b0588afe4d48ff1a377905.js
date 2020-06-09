1f1d75e7298800a632f13438293a3054b156d348
Bon Huynh <bon.huynh@dcyf.wa.gov>
2020-06-08 17:40:37 -0700

    Updates

---
+++ precache-manifest.bd1d69ecf0b0588afe4d48ff1a377905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feed391e2fbbf43675334f89c5952e42",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f15d11a08ea686fa3e64",
+    "url": "/monster-rolodex/static/css/main.865cfde2.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f15d11a08ea686fa3e64",
+    "url": "/monster-rolodex/static/js/main.3f672c28.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
